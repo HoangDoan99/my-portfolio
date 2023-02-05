4450b7c93086e7490a373b4db4588e3ae37bab36
DoanHoang99 <hoangdt@bakco.com.vn>
2023-02-05 11:00:23 +0700

    update config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
-const path = require('path');
+const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/docs",
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
   exportPathMap: async (defaultPathMap) => {
     return defaultPathMap;
   },
   images: {
-    loader: 'akamai',
-    path: '',
+    loader: "akamai",
+    path: "",
   },
 };
 
