13f6022940c87f2512318fa0a5f9d8798b1f5d3c
DoanHoang99 <hoangdt@bakco.com.vn>
2023-02-05 11:08:17 +0700

    update config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/docs",
+  basePath: "/my-portfolio",
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
